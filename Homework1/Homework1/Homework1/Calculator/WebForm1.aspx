93544c03653337415c809d076b84027e0550b99a
Batuhan Sert <32801439+ForteAlgebra@users.noreply.github.com>
2020-02-26 23:13:52 +0300

    Updated
    First update

---
+++ Homework1/Homework1/Homework1/Calculator/WebForm1.aspx
@@ -0,0 +1,107 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Homework1.Calculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Calculator</title>
+    <style type="text/css">
+        .kutu {}
+    </style>
+</head>
+<body style="height: 771px">
+    <form id="form1" runat="server">
+         <div style="height: 522px">
+    <asp:Panel ID="Panel1" runat="server"
+            Width="280px" BackColor="#282828">
+            <table class="style1">
+                <tr>
+                    <td colspan="4">
+                        <asp:TextBox ID="TextBox1" runat="server" Height="60px" Width="263px" BackColor="Gray" BorderColor="Silver" Font-Bold="True" Font-Size="Medium" ForeColor="White" ReadOnly="True">0</asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="Button16" runat="server" Height="60px" Text="C" Width="60px" OnClick="Button16_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="Red"  />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button10" runat="server" Height="60px" 
+                            Text="/" Width="60px" OnClick="Button10_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button11" runat="server" Height="60px" 
+                            Text="x" Width="60px" OnClick="Button11_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button12" runat="server" Height="60px" 
+                            Text="-" Width="60px" OnClick="Button12_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="Button7" runat="server" Height="60px" 
+                            Text="7" Width="60px" OnClick="Button7_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button8" runat="server" Height="60px" 
+                            Text="8" Width="60px" OnClick="Button8_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button9" runat="server" Height="60px" 
+                            Text="9" Width="60px" OnClick="Button9_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td rowspan="2">
+                        <asp:Button ID="Button13" runat="server" Height="120px" 
+                            Text="+" Width="60px" OnClick="Button13_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="Button4" runat="server" Height="60px" 
+                            Text="4" Width="60px" OnClick="Button4_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button5" runat="server" Height="60px" 
+                            Text="5" Width="60px" OnClick="Button5_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button6" runat="server" Height="60px" 
+                            Text="6" Width="60px" OnClick="Button6_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Height="60px" 
+                            Text="1" Width="60px" OnClick="Button1_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button2" runat="server" Height="60px" 
+                            Text="2" Width="60px" OnClick="Button2_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button3" runat="server" Height="60px" 
+                            Text="3" Width="60px" OnClick="Button3_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+                    </td>
+                    <td rowspan="2">
+                        <asp:Button ID="Button14" runat="server" Height="120px"
+                            Text="=" Width="60px" OnClick="Button14_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="Red" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Button ID="Button15" runat="server" Height="60px" 
+                            Text="0" Width="132px" OnClick="Button15_Click" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" />
+
+                    </td>
+                    <td><asp:Button ID="Button17" runat="server" BackColor="#282828" BorderColor="#CCCCCC" BorderStyle="Solid" ForeColor="White" Height="60px" Text="-/+" Width="60px" OnClick="Button17_Click" />
+                    </td>
+                    
+                </tr>
+            </table>
+        </asp:Panel>
+     
+    </div>
+    </form>
+</body>
+</html>
---
+++ Homework1/Homework1/Homework1/Calculator/WebForm1.aspx.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.UI.WebControls;
+
+
+namespace Homework1.Calculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static string operation, secondnumberstring,firstnumberstring;
+        static int firstnumber, secondnumber,  firststringlength, secondstringlength, doubletoint;
+        static double divide_op_result;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "1"; }
+            
+            else
+            { TextBox1.Text = TextBox1.Text + "1"; }
+        }
+        protected void Button15_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "0"; }
+            else
+            { TextBox1.Text = TextBox1.Text + "0"; }
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "2"; }
+            else
+            
+            { TextBox1.Text = TextBox1.Text + "2"; }
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "3"; }
+            else
+            { TextBox1.Text = TextBox1.Text + "3"; }
+        }
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "4"; }
+            
+            else
+            { TextBox1.Text = TextBox1.Text + "4"; }
+        }
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "5"; }
+            else
+            { TextBox1.Text = TextBox1.Text + "5"; }
+        }
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "6"; }
+            else
+            { TextBox1.Text = TextBox1.Text + "6"; }
+        }
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "7"; }
+            else
+            { TextBox1.Text = TextBox1.Text + "7"; }
+        }
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "8"; }
+            else
+            { TextBox1.Text = TextBox1.Text + "8"; }
+        }
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            { TextBox1.Text = "9"; }
+            else
+            { TextBox1.Text = TextBox1.Text + "9"; }
+        }
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            {
+                TextBox1.Text = "0";
+            }
+            else
+            {
+                
+                firststringlength = TextBox1.Text.Length;
+                firstnumberstring = TextBox1.Text;
+                firstnumber = Convert.ToInt32(TextBox1.Text);
+                TextBox1.Text = TextBox1.Text + "+";
+            }
+            operation = "+";
+   
+        }
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            {
+                TextBox1.Text = "-";
+            }
+            else
+            {
+                
+                firststringlength = TextBox1.Text.Length;
+                firstnumberstring = TextBox1.Text;
+                firstnumber = Convert.ToInt32(TextBox1.Text);
+                TextBox1.Text = TextBox1.Text + "-";
+            }
+            operation = "-";
+        }
+
+        protected void Button17_Click(object sender, EventArgs e)
+        {
+            secondstringlength = TextBox1.Text.Length;
+
+            if (operation != null   /*operation == "+" || operation == "-" || operation == "x" || operation == "/"*/)
+            {
+                
+                if (firststringlength + 1 == secondstringlength) //Eğer işlemden sonra sayı gelmediyse kontrolü ex: 12+ dedikten sonra işaret değiştirmeye çalışırsa
+                {
+                    TextBox1.Text = TextBox1.Text;//hiçbir şey yapma
+                }
+                else
+                {
+                    secondnumberstring = TextBox1.Text.ToString();                            //Textboxdaki değeri stringe ver
+                    secondnumberstring = secondnumberstring.Substring(firststringlength + 1); //ikinci değeri bulmak için stringde ilk değerden ve işlemden sonraki kısmı alınır
+                    secondnumberstring = secondnumberstring.Replace("(", "");//Eğer sayıyı negatif yaptıysak başına ve sonuna koyduğumuz parantezleri kaldırıyor
+                    secondnumberstring = secondnumberstring.Replace(")", "");//
+                    secondnumber = Convert.ToInt32(secondnumberstring);                       //ikinci değeri stringden integera çevirme
+                    if (secondnumber < 0)                                                      //ikinci değer negatif mi pozitif mi
+                    {
+                        secondnumber = -1 * secondnumber;                                         // ikinci değerin işaretini değiştirme
+                        secondnumberstring = Convert.ToString(secondnumber);
+                        TextBox1.Text = firstnumberstring + operation + secondnumberstring;
+                    }
+                    else
+                    {
+                        secondnumber = -1 * secondnumber;                                         // ikinci değerin işaretini değiştirme
+                        secondnumberstring = Convert.ToString(secondnumber);
+                        TextBox1.Text = firstnumberstring + operation + "(" + secondnumberstring + ")";
+                    }
+                    
+                }
+                
+            }
+            else                                                                                //eğer ikinci değer girilmediyse
+            {
+                firstnumber = Convert.ToInt32(TextBox1.Text);                                   //ilk değeri integera çevirme
+
+                if (firstnumber < 0)
+                {
+                                                 
+                    firstnumber = -1 * firstnumber;
+                    firstnumberstring = Convert.ToString(firstnumber);
+                    TextBox1.Text = firstnumberstring;
+                }
+                else
+                {
+                    firstnumber = -1 * firstnumber;
+                    firstnumberstring = Convert.ToString(firstnumber);
+                    TextBox1.Text = firstnumberstring;
+                }
+            }
+   
+        }
+
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            {
+                TextBox1.Text = "0";
+            }
+            else
+            {
+                firststringlength = TextBox1.Text.Length;
+                firstnumberstring = TextBox1.Text;
+                firstnumber = Convert.ToInt32(TextBox1.Text);
+                TextBox1.Text = TextBox1.Text + "x";
+            }
+            operation = "x";
+        }
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text == "0")
+            {
+                TextBox1.Text = "0";
+            }
+            else
+            {
+                
+                firststringlength = TextBox1.Text.Length;
+                firstnumberstring = TextBox1.Text;
+                firstnumber = Convert.ToInt32(TextBox1.Text);
+                TextBox1.Text = TextBox1.Text + "/";
+            }
+            operation = "/";
+
+        }
+
+        protected void Button14_Click(object sender, EventArgs e)
+        {
+            secondnumberstring = TextBox1.Text.ToString();
+            secondnumberstring = secondnumberstring.Substring(firststringlength + 1);
+            secondnumberstring = secondnumberstring.Replace("(", "");//Eğer sayıyı negatif yaptıysak başına ve sonuna koyduğumuz parantezleri kaldırıyor
+            secondnumberstring = secondnumberstring.Replace(")", "");//
+            secondnumber = Convert.ToInt32(secondnumberstring);
+
+
+            if (operation == "+")
+                TextBox1.Text = Convert.ToString(firstnumber + secondnumber);
+            if (operation == "-")
+                TextBox1.Text = Convert.ToString(firstnumber - secondnumber);
+            if (operation == "x")
+                TextBox1.Text = Convert.ToString(firstnumber * secondnumber);
+            if (operation == "/") 
+            {   
+                if (secondnumber == 0)
+                {
+                    TextBox1.Text = "Division-By-Zero Exception";
+                    Response.AddHeader("Refresh", "2");
+
+                    
+                }
+                else
+                {
+                    divide_op_result = Convert.ToDouble(firstnumber) / Convert.ToDouble(secondnumber);
+                    doubletoint = Convert.ToInt32(divide_op_result);
+                    TextBox1.Text = Convert.ToString(divide_op_result);
+                    if (doubletoint != divide_op_result)
+                    {
+                        Response.AddHeader("Refresh", "3");
+                    }
+                   
+                }
+            }
+
+            operation = null;
+
+
+        }
+        
+
+        protected void Button16_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "0";
+            operation = null;
+
+        }
+
+    }
+}
